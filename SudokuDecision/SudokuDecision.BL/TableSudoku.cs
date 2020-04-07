9422f592158b639c5232c19e66fc9bf6944a449c
dimaematveev <dimaematveev@gmail.com>
2020-04-07 21:18:08 +0300

    сделал для судоку нормальную таблицу

---
+++ SudokuDecision/SudokuDecision.BL/TableSudoku.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuDecision.BL
+{
+    public class TableSudoku
+    {
+        private DataTable DataTable;
+        public List<ListSudoku> Rows;
+        public List<ListSudoku> Columns;
+        public List<ListSudoku> Tables;
+
+        public TableSudoku(DataTable dataTable)
+        {
+            DataTable = dataTable;
+            Rows = new List<ListSudoku>();
+            Columns = new List<ListSudoku>();
+            Tables = new List<ListSudoku>();
+        }
+
+        public override string ToString()
+        {
+            string ret = "";
+            ret += "Строки: \n";
+            foreach (var item in Rows)
+            {
+                ret += $"\t{{{item}}} \n";
+            }
+            ret += "Столбцы: \n";
+            foreach (var item in Columns)
+            {
+                ret += $"\t{{{item}}} \n";
+            }
+            ret += "Таблицы 3х3: \n";
+            foreach (var item in Tables)
+            {
+                ret += $"\t{{{item}}}\n";
+            }
+            return ret;
+
+        }
+    }
+
+    public class ListSudoku:IEnumerable
+    {
+       
+        public List<ItemSudoku> ItemSudokus { get; set; }
+        private DataTable DataTable;
+        public ListSudoku(DataTable dataTable)
+        {
+            ItemSudokus = new List<ItemSudoku>();
+            DataTable = dataTable;
+        }
+
+        public void Add(int row, int col)
+        {
+            ItemSudokus.Add(new ItemSudoku(row, col, DataTable));
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ItemSudokus.GetEnumerator();
+        }
+        public override string ToString()
+        {
+            string ret = "";
+            foreach (var item in this)
+            {
+                ret += $"[{item}], ";
+            }
+
+            return ret;
+        }
+    }
+    public class ItemSudoku
+    {
+        public object Item { get => DataTable.Rows[Row][Col]; }
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+        private DataTable DataTable;
+
+        public ItemSudoku(int row, int col,DataTable dataTable)
+        {
+            Row = row;
+            Col = col;
+            DataTable = dataTable;
+        }
+
+        public override string ToString()
+        {
+            return $"c:{Col}_r:{Row}-i:{Item}";
+        }
+    }
+}
--- SudokuDecision/SudokuDecision.WPF/MainWindow.xaml.cs
+++ SudokuDecision/SudokuDecision.WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SudokuDecision.BL;
 
 namespace SudokuDecision.WPF
 {
@@ -20,9 +22,153 @@ namespace SudokuDecision.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        DataTable table = new DataTable();
+        TableSudoku tableSudoku;
+
+        public MainWindow() 
         {
             InitializeComponent();
+
+
+            Loaded += MainWindow_Loaded;
+            
         }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Zapoln();
+
+            ReturnAllRows();
+            ReturnAllColumns();
+            ReturnAllTable();
+
+        }
+
+
+        private void ReturnAllRows()
+        {
+            for (int i = 0; i < table.Columns.Count; i++)
+            {
+                var temp = new ListSudoku(table);
+                for (int j = 0; j < table.Rows.Count; j++)
+                {
+                    temp.Add(j,i);
+                }
+                tableSudoku.Columns.Add(temp);
+            }
+        }
+
+        private void ReturnAllColumns()
+        {
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                var temp = new ListSudoku(table);
+                for (int j = 0; j < table.Columns.Count; j++)
+                {
+                    temp.Add(i,j);
+                   
+                }
+                tableSudoku.Rows.Add(temp);
+            }
+        }
+
+        private void ReturnAllTable()
+        {
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                var temp = new ListSudoku(table);
+                for (int j = 0; j < table.Columns.Count; j++)
+                {
+                    int col = (i / 3) * 3 + j / 3;
+                    int row = (i % 3) * 3 + j % 3;
+                    temp.Add(col,row);
+                }
+                tableSudoku.Tables.Add(temp);
+            }
+        }
+
+        private void Zapoln()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                table.Columns.Add(new DataColumn() { ColumnName = $"c{i}" });
+            }
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                DataRow dataRow = table.NewRow();
+
+                for (int j = 0; j < table.Columns.Count; j++)
+                {
+                  
+                    dataRow[j] = $"r{i}_{table.Columns[j].ColumnName}";
+                }
+               
+                table.Rows.Add(dataRow);
+            }
+           
+
+            Table2.Items.Clear();
+            Table2.ItemsSource = table.DefaultView;
+            Table2.CanUserAddRows = false;
+            Table2.CanUserResizeColumns = false;
+
+            tableSudoku = new TableSudoku(table);
+
+        }
+
+
+
+        ///// <summary>
+        ///// заполнение случайными числами
+        ///// </summary>
+        //private void Zapoln()
+        //{
+        //    foreach (var item1 in Table1.Children)
+        //    {
+        //        if (item1 is Grid)
+        //        {
+
+        //            Grid itemDataGrid = (Grid)item1;
+
+        //            foreach (var item2 in itemDataGrid.Children)
+        //            {
+        //                if (item2 is TextBox)
+        //                {
+        //                    TextBox itemTextBox = (TextBox)item2;
+        //                    itemTextBox.Text = itemTextBox.Name;
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+        ///// <summary>
+        ///// Пробую вывести все столбцы
+        ///// </summary>
+        //private void ReturnAllStold()
+        //{
+        //    string str = "";
+
+        //    foreach (var item1 in Table1.ColumnDefinitions)
+        //    {
+        //        var d = item1;
+        //        var z = d.BindingGroup;
+        //        //if (item1 is Grid)
+        //        //{
+        //        //    Grid itemDataGrid = (Grid)item1;
+        //        //    foreach (var item2 in itemDataGrid.Children)
+        //        //    {
+        //        //        if (item2 is TextBox)
+        //        //        {
+        //        //            TextBox itemTextBox = (TextBox)item2;
+        //        //            itemTextBox.Text = itemTextBox.Name;
+        //        //        }
+        //        //    }
+        //        //}
+        //    }
+
+        //    MessageBox.Show(str);
+        //}
     }
 }
