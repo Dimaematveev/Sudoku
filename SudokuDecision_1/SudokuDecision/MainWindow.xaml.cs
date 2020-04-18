43f7300e052df1269e5c794b75644cbd2e04ddd4
dimaematveev <dimaematveev@gmail.com>
2020-04-19 00:29:48 +0300

    Делаю таблицу потихоньку

--- SudokuDecision_1/SudokuDecision/MainWindow.xaml.cs
+++ SudokuDecision_1/SudokuDecision/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,101 @@ namespace SudokuDecision
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable dataTableSudoku = new DataTable("Sudoku");
         public MainWindow()
         {
             InitializeComponent();
+            
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            int numSudoku = 9;
+            dataTableSudoku.Columns.Add($"N");
+            for (int i = 1; i <= numSudoku; i++)
+            {
+                dataTableSudoku.Columns.Add($"{i}", typeof(char));
+            }
+            for (int i = 1; i <= numSudoku; i++)
+            {
+                var newRow = dataTableSudoku.NewRow();
+                for (int j = 0; j < newRow.ItemArray.Length; j++)
+                {
+                    if (j==0)
+                    {
+                        newRow[j] = i.ToString()[0];
+                    }
+                    
+                    else
+                    {
+                        newRow[j] = ' ';
+                        //newRow[j] = $"{i}_{j}";
+                    }
+
+                }
+                dataTableSudoku.Rows.Add(newRow);
+            }
+            DataGridSudoku.ItemsSource = dataTableSudoku.DefaultView;
+            DataGridSudoku.Columns[0].IsReadOnly = true;
+            DataGridSudoku.CanUserAddRows = false;
+            DataGridSudoku.CanUserResizeColumns = false;
+            DataGridSudoku.CanUserDeleteRows = false;
+            DataGridSudoku.CanUserResizeRows = false;
+            DataGridSudoku.CanUserReorderColumns = false;
+            DataGridSudoku.CanUserSortColumns = false;
+        }
+
+        //private void FillFile(string nameFile)
+        //{
+        //    table.Clear();
+        //    string line;
+        //    StreamReader file = new StreamReader(nameFile);
+        //    while ((line = file.ReadLine()) != null)
+        //    {
+        //        var temp = line.Split(';');
+
+        //        DataRow dataRow = table.NewRow();
+
+        //        for (int j = 0; j < table.Columns.Count; j++)
+        //        {
+
+        //            dataRow[j] = temp[j];
+        //        }
+
+        //        table.Rows.Add(dataRow);
+        //    }
+
+
+        //    Table2.ItemsSource = table.DefaultView;
+        //    Table2.CanUserAddRows = false;
+        //    Table2.CanUserResizeColumns = false;
+        //    Table2.CanUserDeleteRows = false;
+        //    Table2.CanUserResizeRows = false;
+        //    Table2.CanUserReorderColumns = false;
+        //    Table2.CanUserSortColumns = false;
+        //}
+
+
+        //private void OpenFile_Click(object sender, RoutedEventArgs e)
+        //{
+        //    OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        //    openFileDialog1.Filter = "All files(*.*)|*.*";
+        //    openFileDialog1.ShowDialog();
+
+
+        //    FillFile(openFileDialog1.FileName);
+        //}
+    }
+
+    public class ForDataGrid
+    {
+        public string Name;
+
+        public ForDataGrid(string name)
+        {
+            Name = name;
         }
     }
 }
