6f2479894d78120c10dbbefd8dd8db36d64e6107
dimaematveev <dimaematveev@gmail.com>
2020-04-19 14:04:42 +0300

    Данные из csv

--- SudokuDecision_1/SudokuDecision/MainWindow.xaml.cs
+++ SudokuDecision_1/SudokuDecision/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,54 +28,20 @@ namespace SudokuDecision
         {
             InitializeComponent();
             
-
             Loaded += MainWindow_Loaded;
-            DataGridSudoku.CellEditEnding += DataGridSudoku_CellEditEnding1;
+            DataGridSudoku.CellEditEnding += DataGridSudoku_CellEditEnding;
+            ButtonOpenFile.Click += (sender, e) => { OpenFile_Click(); };
         }
 
-        private void DataGridSudoku_CellEditEnding1(object sender, DataGridCellEditEndingEventArgs e)
-        {
-            TextBox cell = (TextBox)e.EditingElement;
-            if (cell.Text.Length > 1)
-            {
-                throw new ArgumentException("Не символ");
-            }
-            else
-            {
-                char k = cell.Text[0];
-                dataTableSudoku.Rows[e.Row.GetIndex()][e.Column.DisplayIndex] = new ItemCellSudoku() { Item = k };
-                //e.EditingElement= new ItemCellSudoku() { Item = k };
-                //((DataGrid)sender).SelectedItem= new ItemCellSudoku() { Item = k };
-            }
-           
-            
-        }
-
-        private void DataGridSudoku_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
-        {
-
-            if (e.EditingElement is TextBox)
-            {
-                TextBox cell = (TextBox)e.EditingElement;
-                if (cell.Text.Length > 1) 
-                {
-                    throw new ArgumentException("Не символ");
-                }
-                else
-                {
-                    char k = cell.Text[0];
-                    //e.EditingElement= new ItemCellSudoku() { Item = k };
-                    //((DataGrid)sender).SelectedItem= new ItemCellSudoku() { Item = k };
-                }
-            }
-            else
-            {
-                throw new ArgumentException("Не текстбокс");
-            }
-            
-        }
-
+       
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            NewTable();
+            FillFile(@"D:\Дмитрий\Documents\Настраиваемые шаблоны Office\sud1.csv");
+
+        }
+
+        private void NewTable()
         {
             int numSudoku = 9;
             for (int i = 0; i < numSudoku; i++)
@@ -85,8 +53,8 @@ namespace SudokuDecision
                 var newRow = dataTableSudoku.NewRow();
                 for (int j = 0; j < newRow.ItemArray.Length; j++)
                 {
-                    
-                    newRow[j] = new ItemCellSudoku() { Item = ' ' }; 
+
+                    newRow[j] = new ItemCellSudoku() { Item = ' ' };
                     //newRow[j] = $"{i}_{j}";
                 }
                 dataTableSudoku.Rows.Add(newRow);
@@ -98,9 +66,30 @@ namespace SudokuDecision
             DataGridSudoku.CanUserResizeRows = false;
             DataGridSudoku.CanUserReorderColumns = false;
             DataGridSudoku.CanUserSortColumns = false;
-
-            List<ItemCellSudoku> ListUnion = new List<ItemCellSudoku>();
         }
+
+        private void DataGridSudoku_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            TextBox cell = (TextBox)e.EditingElement;
+            if (cell.Text.Length > 1)
+            {
+                throw new ArgumentException("Не символ");
+            }
+            else
+            {
+                char k;
+                if (cell.Text.Length == 0)
+                {
+                    k = ' ' ;
+                }
+                else
+                {
+                    k = cell.Text[0];
+                }
+                dataTableSudoku.Rows[e.Row.GetIndex()][e.Column.DisplayIndex] = new ItemCellSudoku() { Item = k };
+            }
+        }
+
 
         /* TODO: что надо примерно сделать
          * Первое создать список всех возможных объединений где не должны совпадать элементы
@@ -108,55 +97,48 @@ namespace SudokuDecision
          *
          *
          */
-        //private void FillFile(string nameFile)
-        //{
-        //    table.Clear();
-        //    string line;
-        //    StreamReader file = new StreamReader(nameFile);
-        //    while ((line = file.ReadLine()) != null)
-        //    {
-        //        var temp = line.Split(';');
-
-        //        DataRow dataRow = table.NewRow();
-
-        //        for (int j = 0; j < table.Columns.Count; j++)
-        //        {
-
-        //            dataRow[j] = temp[j];
-        //        }
-
-        //        table.Rows.Add(dataRow);
-        //    }
-
-
-        //    Table2.ItemsSource = table.DefaultView;
-        //    Table2.CanUserAddRows = false;
-        //    Table2.CanUserResizeColumns = false;
-        //    Table2.CanUserDeleteRows = false;
-        //    Table2.CanUserResizeRows = false;
-        //    Table2.CanUserReorderColumns = false;
-        //    Table2.CanUserSortColumns = false;
-        //}
-
-
-        //private void OpenFile_Click(object sender, RoutedEventArgs e)
-        //{
-        //    OpenFileDialog openFileDialog1 = new OpenFileDialog();
-        //    openFileDialog1.Filter = "All files(*.*)|*.*";
-        //    openFileDialog1.ShowDialog();
-
-
-        //    FillFile(openFileDialog1.FileName);
-        //}
-    }
-
-    public class ForDataGrid
-    {
-        public string Name;
-
-        public ForDataGrid(string name)
+        private void FillFile(string nameFile)
         {
-            Name = name;
+            dataTableSudoku.Clear();
+            string line;
+            StreamReader file = new StreamReader(nameFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                var temp = line.Split(';');
+                DataRow dataRow = dataTableSudoku.NewRow();
+                for (int j = 0; j < dataTableSudoku.Columns.Count; j++)
+                {
+                    if (temp[j].Length>1)
+                    {
+                        throw new ArgumentException("Не символ");
+                    }
+                    else
+                    {
+                        char k;
+                        if (temp[j].Length == 0)
+                        {
+                            k = ' ';
+                        }
+                        else
+                        {
+                            k = temp[j][0];
+                        }
+                        dataRow[j] = new ItemCellSudoku() { Item = k };
+                    }
+                }
+                dataTableSudoku.Rows.Add(dataRow);
+            }
+        }
+
+
+        private void OpenFile_Click()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "All files(*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            FillFile(openFileDialog1.FileName);
         }
     }
+
+    
 }
